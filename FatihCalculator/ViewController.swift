ee397f2a429a95f49625b5ac13f47e5139489cbd
Fatih inan <fatihinan@Fatihs-Mac.local>
2017-09-24 16:58:21 -0700

    Initial Commit

---
+++ FatihCalculator/ViewController.swift
@@ -0,0 +1,74 @@
+/*
+ * Name: Mehmet Fatih Inan
+ * Date: 2017/09/24
+ * StudentID: 300966544
+ * Description: Calculator
+ * Version: 0.1 Built screen
+ */
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    
+    
+    @IBAction func zero(_ sender: UIButton) {
+    }
+    @IBAction func one(_ sender: UIButton) {
+    }
+    
+    @IBAction func two(_ sender: UIButton) {
+    }
+
+    @IBAction func three(_ sender: UIButton) {
+    }
+    
+    @IBAction func four(_ sender: UIButton) {
+    }
+    
+    @IBAction func five(_ sender: UIButton) {
+    }
+    
+    @IBAction func six(_ sender: UIButton) {
+    }
+    
+    @IBAction func seven(_ sender: UIButton) {
+    }
+    
+    @IBAction func eight(_ sender: UIButton) {
+    }
+    
+    @IBAction func nine(_ sender: UIButton) {
+    }
+    
+    @IBAction func dot(_ sender: UIButton) {
+    }
+    
+    @IBAction func equal(_ sender: UIButton) {
+    }
+    
+    @IBAction func add(_ sender: UIButton) {
+    }
+    
+    @IBAction func subtract(_ sender: UIButton) {
+    }
+    
+    @IBAction func multiply(_ sender: UIButton) {
+    }
+    
+    @IBAction func divide(_ sender: UIButton) {
+    }
+    
+    @IBAction func minus(_ sender: UIButton) {
+    }
+    
+    @IBAction func clear(_ sender: UIButton) {
+    }
+        
+    @IBOutlet weak var result: UILabel!
+}
+
